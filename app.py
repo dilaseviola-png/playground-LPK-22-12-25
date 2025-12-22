00e4bc1e2bc93480a51ddd537e0de7fa5a2774d2
dilaseviola-png <dilaseviola@gmail.com>
2025-12-22 14:49:58 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,17 +2,17 @@ import streamlit as st
 
 # st.write("Hello, *World!* :sunglasses:")
 
-st.set_page_config(
-    page_title="Ex-stream-ly Cool App",
-    page_icon="🦈",
-    layout="wide",
-    initial_sidebar_state="expanded",
-    menu_items={
-        'Get Help': 'https://www.extremelycoolapp.com/help',
-        'Report a bug': "https://www.extremelycoolapp.com/bug",
-        'About': "# This is a header. This is an *extremely* cool app!"
-    }
-)
+# st.set_page_config(
+#     page_title="Ex-stream-ly Cool App",
+#     page_icon="🦈",
+#     layout="wide",
+#     initial_sidebar_state="expanded",
+#     menu_items={
+#         'Get Help': 'https://www.extremelycoolapp.com/help',
+#         'Report a bug': "https://www.extremelycoolapp.com/bug",
+#         'About': "# This is a header. This is an *extremely* cool app!"
+#     }
+# )
 # app.py
 import streamlit as st
 
